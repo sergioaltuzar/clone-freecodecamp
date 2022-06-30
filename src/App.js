1df06f5d728c9f36226fa33a8a67ae2c510676be
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-06-29 22:27:10 -0500

    estilos css

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import Testimonio from './componentes/Testimonio';
 
 function App() {
   return (
-    <div className="App">
-      <Testimonio />
+    <div className='App'>
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <Testimonio />
+      </div>
     </div>
   );
 }
