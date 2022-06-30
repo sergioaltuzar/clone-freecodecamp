160ba6fc99410b54670739e368118600d9d41625
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-06-29 21:45:53 -0500

    import testimonio

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -7,6 +7,12 @@ function Testimonio (){
       className="imagen-testimonio" 
       src={require('../imagenes/testimonio-emma.png')}
       alt='foto de emma'/>
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'> Emma Bostian en Suecia</p>
+        <p className='cargo-testimonio'> Ingeniera de Software en Spotify</p>
+        <p className='texto-testimonio'> "Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
+
+      </div>
     </div>
   );
 }
