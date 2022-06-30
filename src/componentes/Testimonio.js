f8d6e0605411f0c703cccaae31404e858caacaec
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-06-30 13:19:37 -0500

    tres testimonios

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -6,13 +6,15 @@ function Testimonio (props){
     <div className='contenedor-testimonio'>
       <img 
       className="imagen-testimonio" 
-      src={require('../imagenes/testimonio-emma.png')}
+      src={require(`../imagenes/testimonio-${props.imagen}.png`)}
       alt='foto de emma'/>
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'> {props.nombre} en {props.pais}</p>
-        <p className='cargo-testimonio'> {props.cargo} en {props.empresa}</p>
-        <p className='texto-testimonio'>"{props.testimonio}"</p>
-
+        <p className='nombre-testimonio'> 
+          <strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className='cargo-testimonio'>
+           {props.cargo} en <strong>{props.empresa}</strong></p>
+        <p className='texto-testimonio'>
+          "{props.testimonio}"</p>
       </div>
     </div>
   );
