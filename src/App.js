ef1e4822ca9f1b48beda705d54bc09b07442900f
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-06-30 09:55:48 -0500

    props ema

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ function App() {
     <div className='App'>
       <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        <Testimonio />
+        <Testimonio 
+        nombre='Emma Bostian'
+        pais='Suecia'
+        imagen=''
+        cargo='Ingeniera de Software'
+        empresa='Spotify'
+        testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
+        />
       </div>
     </div>
   );
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,7 +1,7 @@
 import React from "react";
-import '../css/Testimonio.css';
+import '../hojas-de-estilo/Testimonio.css';
 
-function Testimonio (){
+function Testimonio (props){
   return (
     <div className='contenedor-testimonio'>
       <img 
@@ -9,9 +9,9 @@ function Testimonio (){
       src={require('../imagenes/testimonio-emma.png')}
       alt='foto de emma'/>
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'> Emma Bostian en Suecia</p>
-        <p className='cargo-testimonio'> Ingeniera de Software en Spotify</p>
-        <p className='texto-testimonio'> "Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
+        <p className='nombre-testimonio'> {props.nombre} en {props.pais}</p>
+        <p className='cargo-testimonio'> {props.cargo} en {props.empresa}</p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
 
       </div>
     </div>
